e98488707bc58a592ce9a344a077ac31eab25e8a
Thiago <programadorthiagolopes@gmail.com>
2023-05-25 19:28:22 -0300

    feat: Stuy Schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    count = 0
+    for s, f in permanence_period:
+        if (
+            not all(isinstance(val, int) for val in (s, f))
+            or s is None
+            or f is None
+        ):
+            return None
+        if s <= target_time <= f:
+            count += 1
+    return count
